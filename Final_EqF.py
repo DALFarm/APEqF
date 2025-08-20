8346541bf2a6d47984719cef5124c5a5e85a25de
DALFarm <99andrewlee@snu.ac.kr>
2025-08-20 16:03:52 +0900

    Add files via upload

--- Final_EqF.py
+++ Final_EqF.py
@@ -1,5 +1,7 @@
 from scipy.linalg import expm
+from Symmetry import *
+from pylie import SE23
 
-def continous_lift(xi : State, U : InputSpace) -> np.ndarray:
-    L = np.zeros((21,1))
+def continuous_lift(xi : State, U : InputSpace) -> np.ndarray:
+    L = np.zeros((21, 1))
     L[]
--- Symmetry.py
+++ Symmetry.py
@@ -19,6 +19,16 @@ def numericalDifferential(f, x) -> np.ndarray:
 def blockDiag(A : np.ndarray, B : np.ndarray) -> np.ndarray:
     return np.block([[A, np.zeros((A.shape[0], B.shape[1]))],[np.zeros((B.shape[0], A.shape[1])), B]])
     
+def Gamma(C: SE23) -> SO3:
+    # 회전만 추출
+    return C.R()
+
+def chi(C: SE23) -> SE3:
+    # χ(X) = (A, a)  — a는 보통 C.x()에 들어있는 '속도' 벡터
+    R = C.R()
+    a = C.x().as_vector()   # velocity
+    return SE3.from_R_t(R, a)
+    
 # Coordinate selection, by default normal coordinates will be used,
 # if exponential coordinates are selected remember to not use C_star
 exponential_coords = False
@@ -33,4 +43,11 @@ class symgroup
     
     def __mul__(self, other) -> 'Symgroup': # Define group multiplication
     	assert (isinstance(other, Symgroup))
-    	return Symgroup(self.C * other.C, self.gamma + self.C.as_matrix @ other.gamma @ self.C
+    	return Symgroup(self.C * other.C, self.gamma + self.C.as_matrix @ other.gamma @ self.C.inv().as_matrix(), self.delta +  Gamma(X) * other.delta, self.E * other.E)
+    	
+    @staticmethod
+    def identity():
+    	return Symgroup(SE23.identity(), np.zeros((4, 4)), np.zeros((3, 3)), SO3.identity())
+    	
+    def inv(self) -> 'Symgroup':
+    	return (self.C.inv(),
